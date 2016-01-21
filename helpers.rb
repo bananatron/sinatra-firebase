a6cb2079b07fbf25fd617802d87f23e68e656fc0
Spenser Coke <bananatron@users.noreply.github.com>
2016-01-20 17:20:35 -0800

    Update helper language

--- helpers.rb
+++ helpers.rb
@@ -2,7 +2,8 @@
 #// HELPERS //
 #////////////
 
-#Some example of firebase methods - more info @ http://github.com/oscardelben/firebase-ruby
+# Some example of firebase methods
+# More info @ http://github.com/oscardelben/firebase-ruby
 
 #Example GET from firebase
 def getFirebaseExample
@@ -11,13 +12,13 @@ end
 
 #Example SET from firebase
 def setFirebaseExample
-  hh = {lol: 1, blah: 2}
-  $fb_root.set("/key", hh) #Sets data (will override)
+  hh = {lol: 'string lol', blah: 2}
+  $fb_root.set("/path", hh) #Sets data (will override)
 end
 
 #Example PUSH from firebase (will add, not override)
 #Push adds to a firebase collection (object) and returns the generated ID
 def pushFirebaseExample 
-  hash = {lol: 1, blah: 2}
+  hash = {lol: 'string lol', blah: 50}
   $fb_root.push('/things', hash) #Will return id Firebase generates
 end
